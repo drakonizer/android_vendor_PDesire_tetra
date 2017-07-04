28533a7ab886a675a2fa543482172487fee62ec0
Nikhil Punathil <nikhilpe@gmail.com>
2017-07-04 22:26:05 +0530

    vendor: tetra: normalify

--- tetra-vendor.mk
+++ tetra-vendor.mk
@@ -15,7 +15,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-LOCAL_PATH := vendor/PDesire/tetra
+LOCAL_PATH := vendor/sony/tetra
 
 # etc/permissions
 PRODUCT_COPY_FILES += \
